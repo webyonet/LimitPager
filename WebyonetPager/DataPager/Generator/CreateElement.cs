f39ea2b1c580bd959531d06a42f1cacce7640170
webyonet <webyonet@hotmail.com>
2012-12-16 19:24:45 +0200

    ok

---
+++ WebyonetPager/DataPager/Generator/CreateElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Webyonet.DataPager.Generator
+{
+    class CreateElement
+    {
+        private static CreateElement element;
+        private string ReturnElement { get; set; }
+        private CreateElement() { }
+
+        public static CreateElement GetElement()
+        {
+            if (element == null)
+            {
+                element = new CreateElement();
+            }
+            return element;
+        }
+
+        public string ElementGenerator(ElementType type, string ElementClass, string Text, string Link) 
+        {
+            if (type == ElementType.Link)
+            {
+                ReturnElement = this.LinkElementGenerator(ElementClass, Text, Link);
+            }
+            else
+            {
+                ReturnElement = this.TextElementGenerator(ElementClass, Text);
+            }
+            return ReturnElement;
+        }
+
+        private string TextElementGenerator(string ElementClass, string Text)
+        {
+            return "<span title='" + Text + "' class='" + ElementClass + "'>" + Text + "</span>";
+        }
+        private string LinkElementGenerator(string ElementClass, string Text, string Link)
+        {
+            if (!string.IsNullOrEmpty(ElementClass))
+            {
+                return "<a title='" + Text + "' class='" + ElementClass + "' href='" + Link + "'>" + Text + "</a>";
+            }
+            else
+            {
+                return "<a title='" + Text + "' href='" + Link + "'>" + Text + "</a>";
+            }
+        }
+
+        public enum ElementType 
+        { 
+            Link,
+            Text
+        }
+    }
+}
